9b5d9f14285703057a3847ec831f470b8946fb7e
Bruno Faustino <brunofaustino@MacBook-Air-de-Bruno.local>
2026-09-12 00:20:25 +0100

    Fix production site URL environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import { loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
-  const siteURL = env.PUBLIC_SITE_URL?.trim();
+  const siteURL =
+    process.env.PUBLIC_SITE_URL?.trim() ||
+    env.PUBLIC_SITE_URL?.trim();
 
   return {
     site: siteURL || undefined,
